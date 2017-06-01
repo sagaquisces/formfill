f224b8bbf0995196ef7bdb44dc5a371039cd2896
Michael Dunlap <sagaquisces@gmail.com>
2017-05-31 20:28:21 -0700

    changed script.js to be cleaner, but there appears to be errors in jQuery that need to be resolved.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,15 @@
 $(document).ready(function() {
-  $("#formOne").submit(function(event) {
-    $(".person1").append("blah blah");
-    $(".person2").append("blah blah");
-    $(".animal").append("blah blah");
-    $(".exclamation").append("blah blah");
-    $(".verb").append("blah blah");
-    $(".noun").append("blah blah");
+  $("#blanks form").submit(function() {
+    var blanks = ["exclamation", "adverb", "noun", "adjective"];
+
+    blanks.forEach(function(blank) {
+      var userInput = $("input#" + blank).val();
+      $(this).text(userInput).val();
+    });
 
     $("#story").show();
 
     event.preventDefault();
+
   });
 });
